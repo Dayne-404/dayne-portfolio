04db8ecafc04c9678b210c17562ac15f04b63083
Dayne Dellaire <daynedell@gmail.com>
2024-04-26 19:24:47 -0400

    Finished hero and navbar styling
    +Hero Buttons
    +Various layout fixes
    +Navbar burger menu
    
    TODO:
    -Add background SVG
    -Fix Navbar on laptop scale

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,52 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+
 import './hero.css';
 import profilePicture from '../assets/pfp.jpg';
 
 function Hero() {
 	return (
 		<div className="hero content-container">
-            <div className="hero-img">
-                <img src={profilePicture} alt="image of Dayne" />
-            </div>
-            <div className="hero-text">
-                <p className='hero-greeting'>Hey I'm</p>
-                <h1 className="hero-name text-purple-gradient">Dayne Dellaire</h1>
-                <p className='hero-bio'>
-                    I'm an aspiring software engineer from Toronto, Canada, and a
-                    recent graduate in Computer Science.
-                </p>
-            </div>
+			<div className="hero-content">
+				<div className="hero-img">
+					<img src={profilePicture} alt="image of Dayne" />
+				</div>
+				<div className="hero-text">
+					<p className="hero-greeting">Hey I'm</p>
+					<h1 className="hero-name text-purple-gradient">
+						Dayne Dellaire
+					</h1>
+					<p className="hero-bio">
+						I'm an aspiring software engineer from Toronto, Canada,
+						and a recent graduate in Computer Science.
+					</p>
+				</div>
+				<div className="hero-buttons">
+					<button className="text-color">
+						<FontAwesomeIcon
+							icon={faDownload}
+							className="text-color"
+						/>{' '}
+						Resume
+					</button>
+					<a href="https://github.com/Dayne-404" target="_blank">
+						<FontAwesomeIcon
+							icon={faGithub}
+							className="secondary-text-color"
+						/>
+					</a>
+					<a
+						href="https://ca.linkedin.com/in/dayne-dellaire-954916291"
+						target="_blank"
+					>
+						<FontAwesomeIcon
+							icon={faLinkedin}
+							className="secondary-text-color"
+						/>
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 }
