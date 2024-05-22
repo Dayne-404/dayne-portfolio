fc17180424b7b1f97dab960aa8aef14e9b8c8e43
Dayne Dellaire <daynedell@gmail.com>
2024-05-22 16:20:05 -0400

    updated text and started photo gallery
    +updated the about section text
    +started work on dynamic photo gallery where photos will change over a set time frame

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,22 @@
 import Technologies from './Technologies';
-import aboutImg from '../assets/stare.jpg';
 import SectionHeader from './SectionHeader';
+import stareImg from '../assets/stare.jpg';
+import pfpImg from '../assets/pfp.jpg';
+import blackjackImg from '../assets/blackjack.png';
 import '../styles/about.css';
 import Reveal from 'react-awesome-reveal';
 import { keyframes } from '@emotion/react';
+import { useState, useEffect } from 'react';
 
 function About() {
+	const images = [
+		stareImg,
+		pfpImg,
+		blackjackImg,
+	];
+	
+	const [currentIndex, setCurrentIndex] = useState<number>(0);
+	
 	const customAnimation = keyframes`
 		from {
 			opacity: 0;
@@ -16,38 +27,59 @@ function About() {
 			transform: translate3d(0, 0, 0);
 		}
 	`;
+	
+	// useEffect(() => {
+	// 	const interalId = setInterval(() => {
+	// 		if(currentIndex + 1 >= images.length) {
+	// 			setCurrentIndex(0);
+	// 		} else {
+	// 			setCurrentIndex(currentIndex + 1);
+	// 		}
+	// 	}, 10000);
+
+	// 	return () => clearInterval(interalId);
+	// }, [currentIndex, images.length]);
+	
 
 	return (
 		<>
 			<div id="about" className="about content-container">
 				<SectionHeader number="01." title="ABOUT" />
 				<Reveal keyframes={customAnimation} fraction={0.3} triggerOnce>
-				<div className="about-content">
-					<div className="about-img">
-						<img
-							src={aboutImg}
-							alt="Dayne doing something he likes"
-						/>
+					<div className="about-content">
+						<div className="about-img">
+							<img
+								src={images[currentIndex]}
+								alt="Dayne doing something he likes"
+							/>
+						</div>
+						<div className="about-text">
+							<h3>Passion</h3>
+							<p>
+								I've studied various programming languages and I
+								love building cool, innovative experiences.
+								Although I may be new to the field, I am a very
+								quick learner with a passion for technology.
+							</p>
+							<h3>Work</h3>
+							<p>
+								Outside of coding I've worked as a lifeguard,
+								where I developed strong leadership and teamwork
+								skills. This experience has taught me to stay
+								calm under high stress situations and working
+								effectively within a team.
+							</p>
+							<h3>Hobbies</h3>
+							<p>
+								When I'm not coding you can find my trying to
+								stay active and step away from the computer,
+								whether that be at a swimming pool or gym,
+								skiing when the weather allows, or getting away
+								from it all and going on a camping trip with
+								some friends.
+							</p>
+						</div>
 					</div>
-					<div className="about-text">
-						<p>
-							During my studies as a{' '}
-							<b>computer science student.</b> I delved into
-							back-end development and realized my nautral flair
-							for UI/UX design. Motivated by this realization, I
-							began learning front-end evelopment alongside my
-							studies
-						</p>
-						<p>
-							I aim to build upon my current back-end knowledge
-							and combine it with my front-end skills, working
-							towards becoming a full-stack developer. I am
-							committed to explore and experiment with
-							technologies in pursuit of building seamless and
-							engaging user experiences.
-						</p>
-					</div>
-				</div>
 				</Reveal>
 			</div>
 			<Reveal keyframes={customAnimation} fraction={0.3} triggerOnce>
